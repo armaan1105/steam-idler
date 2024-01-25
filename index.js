40c49018f120c19eb2410bccccd74174241e9ce5
armaan1105 <155895731+armaan1105@users.noreply.github.com>
2024-01-25 23:57:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ var status7 = 1;  // 1 - online, 7 - invisible
 user7 = new steamUser();
 user7.logOn({"accountName": username7, "password": password7, "twoFactorCode": steamTotp.generateAuthCode(shared_secret7)});
 user7.on('loggedOn', () => {
-        if(user7.steamID != null) console.log(use7.steamID + ' - Successfully logged on');
+        if(user7.steamID != null) console.log(user7.steamID + ' - Successfully logged on');
         user7.setPersona(status7);               
  	user7.gamesPlayed(game7);
 });
